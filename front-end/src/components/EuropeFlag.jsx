d6d8f42fc88de4227a113b1353a0363d635683bb
thiago-Coderr <tiagoafonso110@gmail.com>
2024-04-03 14:31:52 +0530

    Europe flag svg component

---
+++ front-end/src/components/EuropeFlag.jsx
@@ -0,0 +1,11 @@
+import eu from "../img/flag-eu.svg";
+
+function EuropeFlag() {
+  return (
+    <div>
+      <img src={eu} alt="Europe union flag" />
+    </div>
+  );
+}
+
+export default EuropeFlag;
