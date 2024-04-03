31ca1bee709ef4786ecb4d4d0d72eb9b931842ea
thiago-Coderr <tiagoafonso110@gmail.com>
2024-04-03 14:36:38 +0530

    USA flag svg component

---
+++ front-end/src/components/AmericaFlag.jsx
@@ -0,0 +1,11 @@
+import america from "../img/america.svg";
+
+function AmericaFlag() {
+  return (
+    <div>
+      <img src={america} alt="usa" />
+    </div>
+  );
+}
+
+export default AmericaFlag;
