9b49485a821a3560a75d4a77d100cf0633bc09e5
thiago-Coderr <tiagoafonso110@gmail.com>
2024-04-03 14:41:32 +0530

    Australia flag svg component

---
+++ front-end/src/components/AustraliaFlag.jsx
@@ -0,0 +1,11 @@
+import au from "../img/flag-au.svg";
+
+function AustraliaFlag() {
+  return (
+    <div>
+      <img src={au} alt="Australia Flag" />
+    </div>
+  );
+}
+
+export default AustraliaFlag;
