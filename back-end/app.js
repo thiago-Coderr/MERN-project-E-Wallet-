984ecded1b0fc2021dabdc7efa06e40a35160b3b
thiago-Coderr <tiagoafonso110@gmail.com>
2024-04-04 16:01:15 +0530

    My app server

--- back-end/app.js
+++ back-end/app.js
@@ -1,14 +1,22 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const userRouter = require("./routes/user");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+dotenv.config({ path: "./config.env" });
 
 const app = express();
-
 app.use(express.json());
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello form server side", app: "E-wallet" });
-});
+app.use("/auth", userRouter);
 
-const port = 3000;
+mongoose.connect("mongodb://127.0.0.1:27017/authentication");
+
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
