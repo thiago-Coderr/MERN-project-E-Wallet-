f8a8da35411d01d48c8357b210cb6e83a325c357
thiago-Coderr <tiagoafonso110@gmail.com>
2024-04-04 16:54:42 +0530

    Create a new user

---
+++ back-end/routes/user.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const userModel = require("../models/User");
+const jwt = require("jsonwebtoken");
+const nodeMailer = require("nodemailer");
+
+const router = express.Router();
+
+//  Signup--Create a new user router
+router.post("/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+  const user = await userModel.findOne({ email });
+
+  if (user) {
+    return res.json({ status: false, message: "Email already exists" });
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await userModel({ username, email, password: hashPassword });
+  await newUser.save();
+
+  return res.json({ status: true, message: "record registed" });
+});
+
+module.exports = router;
