fc2404ed6564bb8847c27679c52ad1c1e346fb82
thiago-Coderr <tiagoafonso110@gmail.com>
2024-04-05 15:46:41 +0530

    Home page component

---
+++ front-end/src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import Header from "../components/Header";
+import SubHeader from "../components/SubHeader";
+import TertiarySubHeader from "../components/TertiarySubHeader";
+import SecondSubHeader from "../components/SecondSubHeader";
+
+function HomePage() {
+  return (
+    <>
+      <Header />
+      <SubHeader />
+      <SecondSubHeader />
+      <TertiarySubHeader />
+    </>
+  );
+}
+
+export default HomePage;
