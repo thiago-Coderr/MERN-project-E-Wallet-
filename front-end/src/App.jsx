b031a691c22a999fc76db888f83adf82372293c0
thiago-Coderr <tiagoafonso110@gmail.com>
2024-04-05 16:09:58 +0530

    Updated App with all possible routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,25 @@
 import "./App.scss";
-import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
+import Business from "./pages/Business";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import MultiAccount from "./pages/MultiAccount";
+import ForgotPassword from "./pages/ForgotPassword";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/business" element={<Business />} />
+          <Route path="/multi-currency-account" element={<MultiAccount />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
