41cf3f6cc279b71d5e391bdb507aa0659e1ead35
thiago-Coderr <tiagoafonso110@gmail.com>
2024-04-11 14:47:32 +0530

    Payment method icon

---
+++ front-end/src/components/Amazon.jsx
@@ -0,0 +1,11 @@
+import visa from "../img/amazon-pay.svg";
+
+function Amazon() {
+  return (
+    <div>
+      <img src={visa} alt="Amazon pay icon" />
+    </div>
+  );
+}
+
+export default Amazon;
---
+++ front-end/src/components/ApplePay.jsx
@@ -0,0 +1,11 @@
+import applepay from "../img/applepay.svg";
+
+function ApplePay() {
+  return (
+    <div>
+      <img src={applepay} alt="Apple pay icon" />
+    </div>
+  );
+}
+
+export default ApplePay;
