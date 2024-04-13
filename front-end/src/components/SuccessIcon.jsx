99377514b16e3c77325c935b71cb9124aa2f91c7
thiago-Coderr <tiagoafonso110@gmail.com>
2024-04-13 14:45:05 +0530

    Successful transaction icon for send and add ammount

---
+++ front-end/src/components/SuccessIcon.jsx
@@ -0,0 +1,11 @@
+import success from "../img/success.svg";
+
+function SuccessIcon() {
+  return (
+    <div>
+      <img src={success} alt="Success icon" />
+    </div>
+  );
+}
+
+export default SuccessIcon;
