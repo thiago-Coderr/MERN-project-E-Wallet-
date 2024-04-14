92dc0bde42247d9e9d0fa7c460f187ee1843b388
thiago-Coderr <tiagoafonso110@gmail.com>
2024-04-14 15:16:44 +0530

    svg icon arrow left

---
+++ front-end/src/components/ArrowLeft.jsx
@@ -0,0 +1,11 @@
+import arrow from "../img/arrow-left.svg";
+
+function ArrowLeft() {
+  return (
+    <div>
+      <img src={arrow} alt="Arrow left icon" />
+    </div>
+  );
+}
+
+export default ArrowLeft;
