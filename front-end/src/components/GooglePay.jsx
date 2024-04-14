6f00cc2025a753158fb9d351ff331e19b5e1838f
thiago-Coderr <tiagoafonso110@gmail.com>
2024-04-14 15:21:44 +0530

    google pay svg icon component

---
+++ front-end/src/components/GooglePay.jsx
@@ -0,0 +1,11 @@
+import googlepay from "../img/googlepay.svg";
+
+function GooglePay() {
+  return (
+    <div>
+      <img src={googlepay} alt="google pay icon" />
+    </div>
+  );
+}
+
+export default GooglePay;
