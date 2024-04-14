e40c97f43df64abee53eca295fec7e80ffc19bc7
thiago-Coderr <tiagoafonso110@gmail.com>
2024-04-14 15:32:28 +0530

    transfer wise svg icon

---
+++ front-end/src/components/Wise.jsx
@@ -0,0 +1,11 @@
+import transferwise from "../img/transferwise.svg";
+
+function Wise() {
+  return (
+    <div>
+      <img src={transferwise} alt="transferwise icon" />
+    </div>
+  );
+}
+
+export default Wise;
